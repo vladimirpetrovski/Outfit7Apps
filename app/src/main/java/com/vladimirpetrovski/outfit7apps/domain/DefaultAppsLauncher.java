d5ac10f18dee97ba66fbb0b42d6210c61e0737e8
Vladimir <vladimir.petrovski2@gmail.com>
2018-07-04 21:10:22 +0200

    Implement apps launcher and apps repository.

--- app/src/main/java/com/vladimirpetrovski/outfit7apps/domain/DefaultAppsLauncher.java
+++ app/src/main/java/com/vladimirpetrovski/outfit7apps/domain/DefaultAppsLauncher.java
@@ -1,17 +1,30 @@
 package com.vladimirpetrovski.outfit7apps.domain;
 
 import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
 @Singleton
 public class DefaultAppsLauncher implements AppsLauncher {
 
+  private static final String TAG = DefaultAppsLauncher.class.getSimpleName();
+
+  private Context context;
+
   @Inject
   DefaultAppsLauncher(Context context) {
+    this.context = context;
   }
 
   public void launchPackage(String packageName) {
-    //TODO implement
+    try {
+      Intent launchIntentForPackage =
+          context.getPackageManager().getLaunchIntentForPackage(packageName);
+      context.startActivity(launchIntentForPackage);
+    } catch (Exception ex) {
+      Log.e(TAG, ex.getMessage());
+    }
   }
 }
--- app/src/main/java/com/vladimirpetrovski/outfit7apps/domain/DefaultAppsRepository.java
+++ app/src/main/java/com/vladimirpetrovski/outfit7apps/domain/DefaultAppsRepository.java
@@ -1,6 +1,12 @@
 package com.vladimirpetrovski.outfit7apps.domain;
 
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import com.vladimirpetrovski.outfit7apps.data.App;
+import java.util.ArrayList;
 import java.util.List;
 import javax.inject.Inject;
 import javax.inject.Singleton;
@@ -8,13 +14,35 @@ import javax.inject.Singleton;
 @Singleton
 public class DefaultAppsRepository implements AppsRepository {
 
+  private static final String TAG = AppsRepository.class.getSimpleName();
+
+  private Context context;
+
   @Inject
-  DefaultAppsRepository() {
+  DefaultAppsRepository(Context context) {
+    this.context = context;
   }
 
   @Override
   public List<App> getInstalledApps() {
-    //TODO implement
-    return null;
+    PackageManager packageManager = context.getPackageManager();
+    List<PackageInfo> installedPackages = packageManager
+        .getInstalledPackages(PackageManager.GET_CONFIGURATIONS);
+    List<App> outfit7Apps = new ArrayList<>();
+    for (PackageInfo info : installedPackages) {
+      if (info.packageName.startsWith("com.outfit7.")) {
+        ApplicationInfo ai;
+        try {
+          ai = packageManager.getApplicationInfo(info.packageName, PackageManager.GET_META_DATA);
+        } catch (final NameNotFoundException e) {
+          ai = null;
+        }
+        final String applicationName =
+            ai != null ? (String) packageManager.getApplicationLabel(ai) : "(unknown)";
+        outfit7Apps
+            .add(new App(info.packageName, applicationName, info.versionCode, info.versionName));
+      }
+    }
+    return outfit7Apps;
   }
 }
